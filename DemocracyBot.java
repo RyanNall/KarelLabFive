e2263d196f057d7555328711a8ec401f1042e973
RyanNall <31323098+RyanNall@users.noreply.github.com>
2017-09-19 12:54:06 -0700

    Lab 5 Halfway Thru Problem 5

--- DemocracyBot.java
+++ DemocracyBot.java
@@ -14,10 +14,41 @@ public class DemocracyBot extends Robot
     public void fixBallots() {
         for (int x = 5; x > 0; x --){
         moveToNextDirtyBallot();
-        checkIfBallotHasBeepers();
         cleanBallot();
     }
     }
-    public void moveToNextBallot();
+    public void moveToNextDirtyBallot(){
+        while (!nextToABeeper()){
+            move();
+        }
+    }
+    public void cleanBallot(){
+        pickBeeper();
+        cleanBallotRight();
+        cleanBallotLeft();
+    }
+    public void cleanBallotRight(){
+        turnRight();
+        move();
+        pickBeeper();
+        turnLeft();
+        turnLeft();
+        move();
+        turnRight();
+    }
+    public void cleanBallotLeft(){
+        turnLeft();
+        move();
+        pickBeeper();
+        turnRight();
+        turnRight();
+        move();
+        turnLeft();
+    }
+    public void turnRight(){
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
 }
 
